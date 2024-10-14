5758c9256f49b183ba2937bb32cc34f7e6c38d64
Gabriel Badui <gjbadui@gmail.com>
2024-10-14 19:42:03 -0300

    database fixes

--- src/main/java/exam/badui/ExamApplication.java
+++ src/main/java/exam/badui/ExamApplication.java
@@ -1,5 +1,6 @@
 package exam.badui;
 
+import exam.badui.validators.DnaValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
