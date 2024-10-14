9e5c4d68b766d56462aee8329eacbe3e7e4f3b7a
Gabriel Badui <gjbadui@gmail.com>
2024-10-14 20:30:55 -0300

    main app

--- src/main/java/exam/badui/ExamApplication.java
+++ src/main/java/exam/badui/ExamApplication.java
@@ -1,14 +1,37 @@
 package exam.badui;
 
+import exam.badui.services.DnaService;
 import exam.badui.validators.DnaValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ExamApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ExamApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ExamApplication.class, args);
+        //System.out.println("Running...");
 
-        System.out.println("Running...");
+        DnaService dnaService = context.getBean(DnaService.class);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño de la matriz NxN: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] dna = new String[n];
+        System.out.println("Ingrese las filas de ADN");
+
+        for (int i = 0; i < n; i++) {
+            dna[i] = scanner.nextLine();
+        }
+
+        dnaService.analyzeDna(dna);
+
+        scanner.close();
+
+
     }
 }
